aa444473a9e47a4ad327f6d33a1a4ff62f52ab17
Your Name <you@example.com>
2024-02-06 16:16:37 +0100

    continue working with whiles

--- while.py
+++ while.py
@@ -9,18 +9,45 @@
 #     confirmPassword = input("Confirma la contraseña ")
 # else: print("Las contraseñas coinciden")
 # number = 0
-i = 1
-ch = "*"
-sumNumber = 2
-while i > 0:
-    if( i == 9 ):
-        print(ch*i)
-        print("--------")
-        print(ch*i)
-        sumNumber = -2
-        i+= sumNumber
-        continue
-    print(ch*i)
-    i+= sumNumber
-   
- 
+# i = 1
+# ch = "*"
+# sumNumber = 2
+# while i > 0:
+#     if( i == 9 ):
+#         print(ch*i)
+#         print("--------")
+#         print(ch*i)
+#         sumNumber = -2
+#         i+= sumNumber
+#         continue
+#     print(ch*i)
+#     i+= sumNumber
+
+# i = 0
+
+# while i <   100:
+#     i+=1
+#     if i >= 50:
+#         print(i)
+
+# i = 0
+
+# while i < 21:
+#     if i >= 5 and i <= 20 and i != 12:
+#         print(i)
+#     i+=1
+
+# value = input( "Dame un valor")
+
+# while value != 'q':
+#     value = input( "Dame un valor")
+
+# i = 100
+
+# while i>=50:
+#     print(f"i con valor {i} es mayor o igual que 50")
+#     i = i-1
+
+# for i in range(100):
+#     if i % 10 != 0:
+#         print(i)
